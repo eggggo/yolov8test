44196124667d7b5b84b1a2cdbc12a31d9ce58cb8
eggggo <geneliu2009@gmail.com>
2024-02-18 12:58:51 -0500

    idk anymore this is dum

--- App.tsx
+++ App.tsx
@@ -38,8 +38,10 @@ const MIN_KEYPOINT_SCORE = 0.3;
 // For movenet, the size here doesn't matter too much because the model will
 // preprocess the input (crop, resize, etc). For best result, use the size that
 // doesn't distort the image.
-const OUTPUT_TENSOR_WIDTH = 800;
-const OUTPUT_TENSOR_HEIGHT = 800;
+const OUTPUT_TENSOR_WIDTH = 180;
+const OUTPUT_TENSOR_HEIGHT = OUTPUT_TENSOR_WIDTH / (IS_IOS ? 9 / 16 : 3 / 4);
+
+const numClass = 30;
 
 // Whether to auto-render TensorCamera preview.
 const AUTO_RENDER = false;
@@ -108,6 +110,33 @@ export default function App() {
     };
   }, []);
 
+  const preprocess = (source, modelWidth, modelHeight) => {
+    let xRatio, yRatio; // ratios for boxes
+
+    const input = tf.tidy(() => {
+      const img = source;
+
+      // padding image to square => [n, m] to [n, n], n > m
+      const [h, w] = img.shape.slice(0, 2); // get source width and height
+      const maxSize = Math.max(w, h); // get max size
+      const imgPadded = img.pad([
+        [0, maxSize - h], // padding y [bottom only]
+        [0, maxSize - w], // padding x [right only]
+        [0, 0],
+      ]);
+
+      xRatio = maxSize / w; // update xRatio
+      yRatio = maxSize / h; // update yRatio
+
+      return tf.image
+        .resizeBilinear(imgPadded, [modelWidth, modelHeight]) // resize frame
+        .div(255.0) // normalize
+        .expandDims(0); // add batch
+    });
+
+    return [input, xRatio, yRatio];
+  };
+
   const handleCameraStream = async (
     images: IterableIterator<tf.Tensor3D>,
     updatePreview: () => void,
@@ -116,18 +145,45 @@ export default function App() {
     const loop = async () => {
       // Get the tensor and run pose detection.
       const imageTensor = images.next().value as tf.Tensor3D;
-      const imgTensor2 = imageTensor.asType('float32')
-      const imgTensor3 = imgTensor2.expandDims(0);
+      tf.engine().startScope();
+      const [preprocImg, xRatio, yRatio] = preprocess(imageTensor, 800, 800);
       const startTs = Date.now();
-      const preds = await model!.predict(
-        imgTensor3
-      );
-      //post process
 
-      //setPreds(preds);
-      const latency = Date.now() - startTs;
-      setFps(Math.floor(1000 / latency));
-      tf.dispose([imageTensor, imgTensor2, imgTensor3, preds]);
+      const res = model?.execute(preprocImg); // inference model
+      const transRes = res?.transpose([0, 2, 1]); // transpose result [b, det, n] => [b, n, det]
+      const boxes = tf.tidy(() => {
+        const w = transRes.slice([0, 0, 2], [-1, -1, 1]); // get width
+        const h = transRes.slice([0, 0, 3], [-1, -1, 1]); // get height
+        const x1 = tf.sub(transRes.slice([0, 0, 0], [-1, -1, 1]), tf.div(w, 2)); // x1
+        const y1 = tf.sub(transRes.slice([0, 0, 1], [-1, -1, 1]), tf.div(h, 2)); // y1
+        return tf
+          .concat(
+            [
+              y1,
+              x1,
+              tf.add(y1, h), //y2
+              tf.add(x1, w), //x2
+            ],
+            2
+          )
+          .squeeze();
+      }); // process boxes [y1, x1, y2, x2]
+
+      const [scores, classes] = tf.tidy(() => {
+        // class scores
+        const rawScores = transRes.slice([0, 0, 4], [-1, -1, numClass]).squeeze(0); // #6 only squeeze axis 0 to handle only 1 class models
+        return [rawScores.max(1), rawScores.argMax(1)];
+      }); // get max scores and classes index
+
+      const nms = await tf.image.nonMaxSuppressionAsync(boxes, scores, 500, 0.45, 0.2); // NMS to filter boxes
+
+      const boxes_data = boxes.gather(nms, 0).dataSync(); // indexing boxes by nms index
+      const scores_data = scores.gather(nms, 0).dataSync(); // indexing scores by nms index
+      const classes_data = classes.gather(nms, 0).dataSync(); // indexing classes by nms index
+
+      tf.dispose([res, transRes, boxes, scores, classes, nms]);
+
+      tf.engine().endScope();
 
       if (rafId.current === 0) {
         return;
@@ -139,6 +195,8 @@ export default function App() {
         gl.endFrameEXP();
       }
 
+      const latency = Date.now() - startTs;
+      setFps(Math.floor(1000 / latency));
       rafId.current = requestAnimationFrame(loop);
     };
     loop();
